ec51856abfe12238cde81be4e2de92afb81de5e7
Chance Harmon <henrikillsit@gmail.com>
2019-10-09 11:24:24 -0700

    working as an array

--- src/petRouter.js
+++ src/petRouter.js
@@ -5,12 +5,33 @@ const uuid = require('uuid/v4');
 
 const router = express.Router();
 
-let petToys = [
+const pet1 =
   { _id: uuid(), name: 'Conan', toy: 'ball' }
-];
+  ;
+let petToys = [
+  pet1,
+]
 
 router.get('/pets', (request, response) => {
   response.status(200).json(petToys);
-})
+});
+
+router.post('/pets/', (request, response) => {
+  const newPet = {
+    _id: uuid(), name: request.body.name, toy: request.body.toy,
+  };
+  petToys.push(newPet)
+  response.status(200).json(petToys)
+});
+
+router.delete('/pets', (request, response) => {
+  const deleteId = request.body._id;
+  petToys.forEach((pet, idx) => {
+    if (pet._id === deleteId) {
+      petToys.splice(idx, 1);
+    }
+  })
+  response.status(200).json(petToys)
+});
 
 module.exports = router;
