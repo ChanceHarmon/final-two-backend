b804228a22cf12bb56ed9a41e32119e92ee05cf4
Chance Harmon <henrikillsit@gmail.com>
2019-10-09 09:54:33 -0700

    get route working

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const uuid = require('uuid');
+const petRouter = require('./petRouter.js')
 
 const app = express();
 
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(petRouter);
+app.use(petRouter);
 
 module.exports = {
   server: app,
--- src/petRouter.js
+++ src/petRouter.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const express = require('express');
+const uuid = require('uuid/v4');
+
+const router = express.Router();
+
+let petToys = [
+  { _id: uuid(), name: 'Conan', toy: 'ball' }
+];
+
+router.get('/pets', (request, response) => {
+  response.status(200).json(petToys);
+})
+
+module.exports = router;
